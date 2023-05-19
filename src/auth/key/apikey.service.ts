7880fa1b474d84aed243f9eed0a9a72345f2b27b
Bliss Abhademere <blissfelix18@gmail.com>
2023-05-19 17:32:26 +0100

    extra: error handling

--- src/auth/key/apikey.service.ts
+++ src/auth/key/apikey.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { ApiKey } from './apikey.model';
 import { Model } from 'mongoose';
@@ -10,37 +10,57 @@ export class ApiKeyService {
   constructor(@InjectModel(ApiKey.name) private apiKeyModel: Model<ApiKey>) {}
 
   async createApiKey(signupDto: SignupDto): Promise<ApiKey> {
-    const salt = await bcrypt.genSalt();
-    const hashedPassword = await bcrypt.hash(signupDto.password, salt);
-    const createdApiKey = new this.apiKeyModel({
-      key: await bcrypt.hash(`${signupDto.email}${Date.now()}`, salt),
-      name: signupDto.name,
-      email: signupDto.email,
-      password: hashedPassword,
-    });
+    try {
+      const salt = await bcrypt.genSalt();
+      const hashedPassword = await bcrypt.hash(signupDto.password, salt);
+      const createdApiKey = new this.apiKeyModel({
+        key: await bcrypt.hash(`${signupDto.email}${Date.now()}`, salt),
+        name: signupDto.name,
+        email: signupDto.email,
+        password: hashedPassword,
+      });
 
-    return createdApiKey.save();
+      return createdApiKey.save();
+    } catch (error) {
+      throw new Error('Failed to create API key');
+    }
   }
 
   async validateApiKey(apiKey: string): Promise<boolean> {
-    const apiKeyInfo = await this.apiKeyModel.findOne({ key: apiKey });
+    try {
+      const apiKeyInfo = await this.apiKeyModel.findOne({ key: apiKey });
 
-    if (apiKeyInfo) {
-      return true;
-    } else {
-      return false;
+      if (apiKeyInfo) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (error) {
+      throw new NotFoundException('API key validation failed');
     }
   }
 
   async findApiKeyByEmail(email: string): Promise<ApiKey> {
-    return this.apiKeyModel.findOne({ email: email });
+    try {
+      return this.apiKeyModel.findOne({ email: email });
+    } catch (error) {
+      throw new NotFoundException('API key not found');
+    }
   }
 
   async findById(id: string): Promise<ApiKey> {
-    return this.apiKeyModel.findById(id).exec();
+    try {
+      return this.apiKeyModel.findById(id).exec();
+    } catch (error) {
+      throw new NotFoundException('API key not found');
+    }
   }
 
   async comparePassword(apiKey: ApiKey, password: string): Promise<boolean> {
-    return bcrypt.compare(password, apiKey.password);
+    try {
+      return bcrypt.compare(password, apiKey.password);
+    } catch (error) {
+      throw new Error('Failed to compare passwords');
+    }
   }
 }
