efea15747fe0abc16074f0d4957e5f944605253f
Bliss Abhademere <blissfelix18@gmail.com>
2023-06-01 10:24:51 +0100

    new: findapibykey auth

--- src/common/guards/apikey-auth.guard.ts
+++ src/common/guards/apikey-auth.guard.ts
@@ -32,7 +32,7 @@ export class ApiKeyAuthGuard implements CanActivate {
         throw new UnauthorizedException('Invalid Api Key');
       }
 
-      request.apiKey = await this.apiKeyService.findApiKeyByEmail(apiKeyValue);
+      request.apiKey = await this.apiKeyService.findApiKeyByKey(apiKeyValue);
       return true;
     } catch (error) {
       throw new UnauthorizedException(error.message);
