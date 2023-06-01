b92ef78cbe730f79e34eeb499c29ee8ba268cd59
Bliss Abhademere <blissfelix18@gmail.com>
2023-06-01 19:13:15 +0100

    feat: cleanup service implemented

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,21 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { JwtModule } from '@nestjs/jwt';
 import { ApiKey, ApiKeySchema } from './key';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { ApiKeyService } from './key';
+import { CleanupService } from './dbKeyCleanup';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: ApiKey.name, schema: ApiKeySchema }]),
-    JwtModule.register({
-      secret: 'supersecret',
-      signOptions: { expiresIn: '1h' },
-    }),
   ],
-  providers: [AuthService, ApiKeyService],
+  providers: [AuthService, ApiKeyService, CleanupService],
   controllers: [AuthController],
-  exports: [AuthService, ApiKeyService],
+  exports: [AuthService, ApiKeyService, CleanupService],
 })
 export class AuthModule {}
