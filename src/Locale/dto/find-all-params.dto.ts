943daaef198a0103b7c8b3e011dea0f462a7409a
Bliss Abhademere <blissfelix18@gmail.com>
2023-06-01 10:23:36 +0100

    feat: extra filter options

--- src/Locale/dto/find-all-params.dto.ts
+++ src/Locale/dto/find-all-params.dto.ts
@@ -1,4 +1,11 @@
-import { IsOptional, IsNumber, IsString, Min, IsIn } from 'class-validator';
+import {
+  IsOptional,
+  IsNumber,
+  IsString,
+  Min,
+  IsIn,
+  IsArray,
+} from 'class-validator';
 
 export class FindAllParams {
   @IsNumber({}, { message: 'Page must be a number' })
@@ -33,4 +40,62 @@ export class FindAllParams {
   @IsOptional()
   @Min(1)
   populationLt: number;
+
+  //Exact matches for string field
+  @IsString({ message: 'Capital must be a string' })
+  @IsOptional()
+  capital: string;
+
+  @IsString({ message: 'Slogan must be a string' })
+  @IsOptional()
+  slogan: string;
+
+  @IsString({ message: 'Landmass must be a string' })
+  @IsOptional()
+  landmass: string;
+
+  @IsString({ message: 'Dialect must be a string' })
+  @IsOptional()
+  dialect: string;
+
+  @IsString({ message: 'Map must be a string' })
+  @IsOptional()
+  map: string;
+
+  @IsString({ message: 'Map must be a string' })
+  @IsOptional()
+  created_date: string;
+
+  @IsString({ message: 'Map must be a string' })
+  @IsOptional()
+  created_by: string;
+
+  @IsString({ message: 'Latitude must be a string' })
+  @IsOptional()
+  latitude: string;
+
+  @IsString({ message: 'Longitude must be a string' })
+  @IsOptional()
+  longitude: string;
+
+  @IsString({ message: 'Website must be a string' })
+  @IsOptional()
+  website: string;
+
+  //Exact matches for array fields
+  @IsArray({ message: 'Past governors must be an array of strings' })
+  @IsOptional()
+  past_governors: string[];
+
+  @IsArray({ message: 'Borders must be an array of strings' })
+  @IsOptional()
+  borders: string[];
+
+  @IsArray({ message: 'Known for must be an array of strings' })
+  @IsOptional()
+  known_for: string[];
+
+  @IsArray({ message: 'Senatorial Districts must be an array of strings' })
+  @IsOptional()
+  senatorialDistricts: string[];
 }
