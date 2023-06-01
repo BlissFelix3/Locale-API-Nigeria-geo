bba8ae982b367736c00db526462969769c959124
Bliss Abhademere <blissfelix18@gmail.com>
2023-06-01 20:43:37 +0100

    new: removed redis for db

--- src/auth/dbKeyCleanup/cleanup.service.ts
+++ src/auth/dbKeyCleanup/cleanup.service.ts
@@ -1,12 +1,9 @@
-/* This Service clears expired users expired keys information on the database every 2 hours */
+/* This Service clears expired users and keys information on the database every 2 hours */
 import { Injectable } from '@nestjs/common';
 import * as cron from 'node-cron';
-import * as Redis from 'ioredis';
 import { InjectModel } from '@nestjs/mongoose';
 import { ApiKey } from '../key';
 import { Model } from 'mongoose';
-
-const redis = new Redis();
 
 @Injectable()
 export class CleanupService {
@@ -25,15 +22,10 @@ export class CleanupService {
   }
 
   async cleanup() {
-    // Fetch all keys from MongoDB
-    const allApiKeys = await this.apiKeyModel.find();
-
-    // Delete the keys from MongoDB that don't exist in Redis
-    for (const apiKey of allApiKeys) {
-      const exists = await redis.exists(apiKey.key);
-      if (exists !== 1) {
-        await this.apiKeyModel.deleteOne({ _id: apiKey._id });
-      }
-    }
+    // Delete expired keys from MongoDB
+    const result = await this.apiKeyModel.deleteMany({
+      expires: { $lt: Date.now() },
+    });
+    console.log(`Deleted ${result.deletedCount} expired keys.`);
   }
 }
