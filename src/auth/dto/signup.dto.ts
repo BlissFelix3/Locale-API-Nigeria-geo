0822e8659edd321b039be88717a347238da01acd
Bliss Abhademere <blissfelix18@gmail.com>
2023-06-06 13:13:31 +0100

    new: swagger api property

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,15 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty, IsEmail } from 'class-validator';
 
 export class SignupDto {
+  @ApiProperty({ description: 'Name of the user' })
   @IsString({ message: 'Name must be string' })
   @IsNotEmpty({ message: 'name is required!' })
   name: string;
 
+  @ApiProperty({ description: 'Email of the user' })
   @IsString({ message: 'Email must be string' })
   @IsNotEmpty({ message: 'Email is required!' })
   @IsEmail({}, { message: 'Please provide a valid email!' })
   email: string;
 
+  @ApiProperty({ description: 'Password of the user' })
   @IsString({ message: 'Password must be string' })
   @IsNotEmpty({ message: 'Password is required!' })
   password: string;
