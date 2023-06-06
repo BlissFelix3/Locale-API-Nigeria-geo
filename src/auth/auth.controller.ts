aa44e1eeb7b08ba0f25ea25c970acb70867708b4
Bliss Abhademere <blissfelix18@gmail.com>
2023-06-06 13:13:12 +0100

    new: swagger implementation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { SignupDto } from './dto';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiOperation({ summary: 'Sign up' })
+  @ApiResponse({
+    status: 201,
+    description: 'The record has been successfully created.',
+  })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
   @Post('signup')
+  @HttpCode(HttpStatus.CREATED)
   async signup(@Body() signupDto: SignupDto) {
     const apiKey = await this.authService.signup(signupDto);
     return {
