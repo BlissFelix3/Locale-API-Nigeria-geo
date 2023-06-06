dd01db1f37db200a7adb024c0f6e6184e7732906
Bliss Abhademere <blissfelix18@gmail.com>
2023-06-06 13:13:57 +0100

    new: swagger api property

--- src/auth/key/apikey.model.ts
+++ src/auth/key/apikey.model.ts
@@ -1,27 +1,35 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
 import { Document } from 'mongoose';
 import mongoose from 'mongoose';
 
 @Schema()
 export class ApiKey {
+  @ApiProperty({ description: 'Id of the API key' })
   @Prop({ default: () => new mongoose.Types.ObjectId().toHexString() })
   _id: string;
 
+  @ApiProperty({ description: 'Key of the API key' })
   @Prop({ required: true })
   key: string;
 
+  @ApiProperty({ description: 'Name of the user' })
   @Prop({ required: true })
   name: string;
 
+  @ApiProperty({ description: 'Email of the user' })
   @Prop({ required: true })
   email: string;
 
+  @ApiProperty({ description: 'Password of the user' })
   @Prop({ required: true })
   password: string;
 
+  @ApiProperty({ description: 'Creation date of the API key' })
   @Prop({ default: () => Date.now() })
   created: Date;
 
+  @ApiProperty({ description: 'Expiration date of the API key' })
   @Prop({ default: () => Date.now() + 3600 * 1000 }) // expires after 1 hour
   expires: Date;
 }
