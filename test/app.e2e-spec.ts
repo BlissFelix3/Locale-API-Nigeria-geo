5ea125d348e7628d163730263ae57ababc0f4a25
Bliss Abhademere <blissfelix18@gmail.com>
2023-06-26 00:12:34 +0100

    feat: e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -6,19 +6,19 @@ import { AppModule } from './../src/app.module';
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
-  });
+  }, 10000);
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('{"status":true,"msg":"Locale API Application"}');
   });
 });
