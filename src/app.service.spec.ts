fea9b9b046730373486dd3417bc277c9d95301a6
Bliss Abhademere <blissfelix18@gmail.com>
2023-06-26 10:54:47 +0100

    feat: app test

---
+++ src/app.service.spec.ts
@@ -0,0 +1,21 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppService } from './app.service';
+
+describe('AppService', () => {
+  let appService: AppService;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      providers: [AppService],
+    }).compile();
+
+    appService = app.get<AppService>(AppService);
+  });
+
+  describe('getHello', () => {
+    it('should return the greeting message', () => {
+      const result = appService.getHello();
+      expect(result).toEqual({ status: true, msg: 'Locale API Application' });
+    });
+  });
+});
